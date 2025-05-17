104105129431c06b844c2dc4ea5b3f2458a24a05
Ivan Iglesias <i.iglesias.bilbao@gmail.com>
2025-05-17 16:48:51 +0200

    Add workflows for GitHub actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ivan-iglesias.github.io',
+  base: '101-astro-blog',
+});
+
